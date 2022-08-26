fb61190149b2a026542409aa5f278a3240a3f967
IzamaraJordao <89167184+IzamaraJordao@users.noreply.github.com>
2022-08-26 15:25:39 -0300

    26/08

--- frontend/src/login/index.tsx
+++ frontend/src/login/index.tsx
@@ -1,51 +1,34 @@
 import type { NextPage } from "next";
-import { selectAuthState, setAuthState } from "../store/authSlice";
-import { useDispatch, useSelector } from "react-redux";
 import React, { useState } from "react";
-import { wrapper } from "../store/store";
 
 
 
-const Home: NextPage = () => {
+
+import {Title,Input,Form} from '../pages/style'
+
+export default function Home()  {
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
-  
-      
-  // const authState = useSelector(selectAuthState);
-  // const dispatch = useDispatch();
-  //  const getServerSideProps = wrapper.getServerSideProps(
-  //   (store) =>
-  //     async ({ params }) => {
-  //       // we can set the initial state from here
-  //       // we are setting to false but you can run your custom logic here
-  //       await store.dispatch(setAuthState(false)); 
-  //       console.log("State on server", store.getState());
-  //       return {
-  //         props: {
-  //           authState: false,
-  //         },
-  //       };
-  //     }
-  // );
+
   return (
     <>
-    <div className="login-container">
-    {/* <h1>Rede<small>Inn</small>Hotel</h1> */}
-        <h1>Login</h1>
-        <form action="">
+    <div>
+    <div>
+        <Title>Rede<small>Inn</small>Hotel</Title>
+        
+        <Form>
             <label htmlFor="Email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Digite o email"/>
+            <Input type="email" name="email" id="email" placeholder="Digite o email"/>
             <label htmlFor="password">Senha</label>
-            <input type="password" name="password" id="password" placeholder="Digite a senha"/>
-            <input type="submit" value="Entrar" className="btn btn-primary btn-block"/>
-        </form> 
+            <Input type="password" name="password" id="password" placeholder="Digite a senha"/>
+            <Input type="submit" value="Entrar" className="btn btn-primary btn-block"/>
+        </Form> 
     </div>
- 
+    </div>
   </>
   );
 };
 
 
-export default Home;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,17 +1,11 @@
 
-import type { AppProps } from 'next/app'
-import React from 'react'
-// import '../pages/login/index.css'
-import '../components/Menu/menu.css'
+import '../template/common/login/index.css'
+import type { AppProps } from "next/app";
+import { wrapper } from "../store/store";
+import React from "react";
 
-
-
-export default function MyApp({ Component, pageProps }: AppProps) {
- 
-  return (
-    
-  <Component {...pageProps} />
-  )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-
+export default wrapper.withRedux(MyApp);
