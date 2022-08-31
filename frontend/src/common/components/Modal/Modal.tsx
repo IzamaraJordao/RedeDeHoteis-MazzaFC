6c4664a8bb14250c548a0c539e178efe8649e1b1
math3us2021 <matheusbf.mf@gmail.com>
2022-08-31 08:48:17 -0300

    base 31/08

--- frontend/src/common/components/Modal/Modal.tsx
+++ frontend/src/common/components/Modal/Modal.tsx
@@ -3,12 +3,31 @@ import Swal from 'sweetalert2'
 import { Modal, ModalCentral, ModalDireita, ModalEsquerda } from './styled';
 import TextField from '@mui/material/TextField';
 import axios from 'axios';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Interface } from 'node:readline/promises';
+import * as Yup from "yup";
 
+interface ModalProps {
+  id: number;
+  nome: string;
+  cpf: string;
+  email: string;
+  telefone: string;
+  observacao: string;
+}
 
+const schema = Yup.object({
+  nome: Yup.string().required("Nome é obrigatório"),
+  cpf: Yup.string().required("CPF é obrigatório"),
+  email: Yup.string().email("Email inválido").required("Email é obrigatório"),
+  telefone: Yup.string().required("Telefone é obrigatório"),
+});
 
 export default function BasicModal({ onClose }) {
-
-<<<<<<< HEAD
 
   const [hospedes, setHospedes] = React.useState<ModalProps[]>([]);
 
@@ -24,106 +43,102 @@ export default function BasicModal({ onClose }) {
     })
 
 
-  // function handlePost(e: any){
-  //   e.preventDefault();
-  //   axios.post("http://localhost:4000/"  }
-  
+    function close() {
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Hospede não cadastrado!',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      onClose();
+    }
 
-  function close() {
-    Swal.fire({
-      position: 'center',
-      icon: 'error',
-      title: 'Hospede não cadastrado!',
-      showConfirmButton: false,
-      timer: 1500
-    })
-    onClose();
+    const style = {
+      position: 'absolute' as 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: 700,
+      bgcolor: 'background.paper',
+      border: '2px solid #000',
+      boxShadow: 24,
+      p: 4,
+    };
+
+    return (
+
+      <Box>
+        <div>
+          <form>
+            <Modal>
+              <ModalCentral>
+                <ModalEsquerda>
+                  <label>Quarto</label>
+                  <TextField id="outlined-basic" label="Quarto" variant="outlined" />
+                </ModalEsquerda>
+                <div>
+                  <label>Andar</label>
+                  <div>
+                    <TextField id="outlined-basic" type="number" label="Andar" variant="outlined" />
+                  </div>
+                </div>
+              </ModalCentral>
+
+              <ModalCentral>
+                <div>
+                  <label>CPF</label>
+                  <TextField id="cpf" type="number" label="CPF" variant="outlined" maxRows={11} {...register("cpf")} />
+                  <p>{errors.cpf?.message}</p>
+                </div>
+
+                <ModalDireita>
+                  <label>Hospedes</label>
+                  <TextField id="nome" label="Hospedes" variant="outlined"  {...register("nome")} />
+                  <p>{errors.nome?.message}</p>
+                </ModalDireita>
+              </ModalCentral>
+              <ModalCentral>
+                <div>
+                  <label>CHECK IN</label>
+                  <TextField id="outlined-basic" type="date" variant="outlined" />
+                </div>
+                <div>
+                  <label>CHECK OUT</label>
+                  <TextField id="outlined-basic" type="date" variant="outlined" />
+                </div>
+              </ModalCentral>
+              <ModalCentral>
+                <div>
+                  <label>Telefone</label>
+                  <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                </div>
+                <div>
+                  <label>Situação</label>
+                  <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                </div>
+              </ModalCentral>
+              <ModalCentral>
+                <div>
+
+                  <TextField
+                    id="outlined-multiline-static"
+                    label="Observação"
+                    multiline
+                    rows={4}
+                    defaultValue="Observação"
+                  />
+                </div>
+              </ModalCentral>
+
+              <Button color="success" variant="contained" type='submit' >Enviar</Button>
+              {/* <button>Enviar</button> */}
+            </Modal>
+          </form>
+          <Button variant="outlined" color="error" onClick={close}>Voltar</Button>
+        </div>
+      </Box >
+    );
   }
-
-  const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 700,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
-  return (
-    <div className="modal">
-      <div className="container">
-        
-        <Modal>
-          <ModalCentral>
-            <ModalEsquerda>
-              <label>Quarto</label>
-              <TextField id="outlined-basic" label="Quarto" variant="outlined" />
-            </ModalEsquerda>
-            <div>
-              <label>Andar</label>
-              <div>
-                <TextField id="outlined-basic" type="number" label="Andar" variant="outlined" />
-              </div>
-            </div>
-          </ModalCentral>
-
-          <ModalCentral>
-            <div>
-              <label>CPF</label>
-              <TextField id="cpf" type="number" label="CPF" variant="outlined" maxRows={11} {...register("cpf")} />
-              <p>{errors.cpf?.message}</p>
-            </div>
-
-            <ModalDireita>
-              <label>Hospedes</label>
-              <TextField id="nome" label="Hospedes" variant="outlined"  {...register("nome")} />
-              <p>{errors.nome?.message}</p>
-            </ModalDireita>
-          </ModalCentral>
-          <ModalCentral>
-            <div>
-              <label>CHECK IN</label>
-              <TextField id="outlined-basic" type="date" variant="outlined" />
-            </div>
-            <div>
-              <label>CHECK OUT</label>
-              <TextField id="outlined-basic" type="date" variant="outlined" />
-            </div>
-          </ModalCentral>
-          <ModalCentral>
-            <div>
-              <label>Telefone</label>
-              <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            </div>
-            <div>
-              <label>Situação</label>
-              <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            </div>
-          </ModalCentral>
-          <ModalCentral>
-            <div>
-
-              <TextField
-                id="outlined-multiline-static"
-                label="Observação"
-                multiline
-                rows={4}
-                defaultValue="Observação"
-              />
-            </div>
-          </ModalCentral>
-          
-          <Button color="success" variant="contained" type='submit' >Enviar</Button>
-          {/* <button>Enviar</button> */}
-        </Modal>
-      </form>
-      <Button variant="outlined" color="error" onClick={close}>Voltar</Button>
-      </div>
-        </Box >
-    
-  );
 }
 
