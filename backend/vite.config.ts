58785ad00cc499696e66237739b073578f4195dd
Samuel Cardoso <samuel.cardoso@mazzafc.tech>
2022-09-12 15:38:32 -0300

    test: configure coverage

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -1,6 +1,11 @@
-import {defineConfig} from 'vitest/config'
-
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    test:{}
+  test: {
+    exclude: ['**/models/**.InMemory.ts', '**/*.spec.ts', '**/exceptions/**'],
+    passWithNoTests: true,
+    coverage: {
+      provider: 'c8',
+    },
+  },
 })
