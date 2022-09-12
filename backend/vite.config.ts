79b86de1f844b0003bfac264f0948ed6659cd6b5
Samuel Cardoso <samuel.cardoso@mazzafc.tech>
2022-09-12 15:41:10 -0300

    fix: change exclude from test to coverage

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: ['**/models/**.InMemory.ts', '**/*.spec.ts', '**/exceptions/**'],
     passWithNoTests: true,
     coverage: {
+      exclude: ['**/models/**.InMemory.ts', '**/*.spec.ts', '**/exceptions/**'],
       provider: 'c8',
     },
   },
