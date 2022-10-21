533d39d1374afbf4d084c0f21ebc77931ff71e3b
IzamaraJordao <89167184+IzamaraJordao@users.noreply.github.com>
2022-10-21 09:54:17 -0300

    feat:select do employee

--- frontend/src/Employees/components/ModalEmployees/Modal.tsx
+++ frontend/src/Employees/components/ModalEmployees/Modal.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { ModalCentral, InputNomeModal, ModalExterna } from './styled';
-import TextField from '@mui/material/TextField';
+import React, { useEffect } from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import { ModalCentral, InputNomeModal, ModalExterna } from './styled'
+import TextField from '@mui/material/TextField'
 import Swal from 'sweetalert2'
-import axios from 'axios';
-import { useForm } from "react-hook-form";
-import { handleRequest } from '../../../api';
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
+import { handleRequest } from '../../../api'
+import { MenuItem, Select } from '@mui/material'
+import { Hotel } from '../../../store/hotelSlice'
 
 const style = {
   position: 'absolute',
@@ -19,84 +21,88 @@ const style = {
   border: '2px solid #000',
   boxShadow: 24,
   p: 4,
-};
+}
 
 export type TypeEmployees = {
-  name: string,
-  rg: string,
-  cpf: string,
-  email: string,
-  phone: string,
-  password: string,
+  name: string
+  rg: string
+  cpf: string
+  email: string
+  phone: string
+  password: string
   address: {
-    street: string,
-    number: string,
-    complement: string,
-    neighborhood: string,
-    city: string,
-    state: string,
+    street: string
+    number: string
+    complement: string
+    neighborhood: string
+    city: string
+    state: string
     zipCode: string
-  },
-  hotel_id: string,
+  }
+  hotel_id: string
 }
 
 export default function ModalEmployee({ onClose }) {
-  const [age, setAge] = React.useState('');
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
-
-  const { register, setValue, handleSubmit, formState: { errors } } = useForm<TypeEmployees>();
-
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TypeEmployees>()
+ const [hotels, setHotels] = React.useState<Pick<Hotel,'id'| 'name'>[]>([{id: 'teste', name: 'teste'}])
   const onSubmit = (data: TypeEmployees) => {
     // handleRequest({ method: 'post', url: '/employee', data },
     // console.log)
-    axios.post('http://localhost:3030/employee', {
-      name: data.name,
-      rg: data.rg,
-      cpf: data.cpf,
-      email: data.email,     
-      phone: data.phone,
-      password: data.password,
-      address: {
-        street: data.address.street,
-        number: data.address.number,
-        complement: data.address.complement,
-        neighborhood: data.address.neighborhood,
-        city: data.address.city,
-        state: data.address.state,
-        zipCode: data.address.zipCode
-      },
-      hotel_id: data.hotel_id
-    })
+    axios
+      .post('http://localhost:3030/employee', {
+        name: data.name,
+        rg: data.rg,
+        cpf: data.cpf,
+        email: data.email,
+        phone: data.phone,
+        password: data.password,
+        address: {
+          street: data.address.street,
+          number: data.address.number,
+          complement: data.address.complement,
+          neighborhood: data.address.neighborhood,
+          city: data.address.city,
+          state: data.address.state,
+          zipCode: data.address.zipCode,
+        },
+        hotel_id: data.hotel_id,
+      })
       .then(() => {
         Swal.fire({
           icon: 'success',
           title: 'Funcionário cadastrado com sucesso!',
           showConfirmButton: false,
-          timer: 1500
+          timer: 1500,
         })
       })
-    onClose();
+    onClose()
   }
+  useEffect(() => {
 
 
+  })
+  function handleChange(e) {
+    setValue('hotel_id', e.target.value)
+
+    
+  }
+  console.log(handleChange)
 
   function handleCep(cep: string) {
-
     if (cep.length === 8) {
-      axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-        .then(response => {
-          setValue('address.street', response.data.logradouro)
-          setValue('address.neighborhood', response.data.bairro)
-          setValue('address.city', response.data.localidade)
-          setValue('address.state', response.data.uf)
-        })
+      axios.get(`https://viacep.com.br/ws/${cep}/json/`).then((response) => {
+        setValue('address.street', response.data.logradouro)
+        setValue('address.neighborhood', response.data.bairro)
+        setValue('address.city', response.data.localidade)
+        setValue('address.state', response.data.uf)
+      })
     }
-
   }
-
 
   function close() {
     Swal.fire({
@@ -104,127 +110,231 @@ export default function ModalEmployee({ onClose }) {
       icon: 'error',
       title: 'Funcionario não cadastrado!',
       showConfirmButton: false,
-      timer: 1500
+      timer: 1500,
     })
-    onClose();
+    onClose()
   }
-
 
 
   return (
     <ModalExterna>
       <Box sx={style}>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <Typography id="modal-modal-title" variant="h4" component="h2" color={'var(--text)'}>
+          <Typography
+            id="modal-modal-title"
+            variant="h4"
+            component="h2"
+            color={'var(--text)'}
+          >
             Cadastro de Funcionário
           </Typography>
         </div>
         <div>
           <form onSubmit={handleSubmit(onSubmit)}>
             <InputNomeModal>
-              <label style={{marginLeft:'10px', color: 'var(--text)'}}>Nome</label>
+              <label style={{ marginLeft: '10px', color: 'var(--text)' }}>
+                Nome
+              </label>
               <TextField
-                size='small' id="name" sx={{width: '757px'}}  {...register("name")} />
+                size="small"
+                id="name"
+                sx={{ width: '757px' }}
+                {...register('name')}
+              />
             </InputNomeModal>
 
             <ModalCentral>
               <div>
                 <InputNomeModal>
                   <label>CPF</label>
-                  <TextField size='small' id="cpf" type="number" variant="outlined" maxRows={11} {...register("cpf")} />
+                  <TextField
+                    size="small"
+                    id="cpf"
+                    type="number"
+                    variant="outlined"
+                    maxRows={11}
+                    {...register('cpf')}
+                  />
                 </InputNomeModal>
               </div>
               <div>
                 <InputNomeModal>
                   <label>RG</label>
-                  <TextField size='small' id="rg" type="text" variant="outlined" maxRows={11} {...register("rg")} />
+                  <TextField
+                    size="small"
+                    id="rg"
+                    type="text"
+                    variant="outlined"
+                    maxRows={11}
+                    {...register('rg')}
+                  />
                 </InputNomeModal>
               </div>
 
               <div>
                 <InputNomeModal>
                   <label>Telefone</label>
-                  <TextField size='small' id="phone" type="number" variant="outlined" {...register("phone")} />
-                </InputNomeModal>
-              </div>
-
-            </ModalCentral>
-            <ModalCentral>
-              <div>
-              <InputNomeModal>
-                <label>Email</label>
-                <TextField sx={{ width: '500px', color: 'var(--text)' }} size='small' id="email" variant="outlined" {...register("email")} />
-                </InputNomeModal>
-              </div>
-              <div>
-              <InputNomeModal>
-                <label>Senha</label>
-                <TextField size='small' type="number" variant="outlined" maxRows={11} {...register("password")} />
-                </InputNomeModal>
-              </div>
-            </ModalCentral>
-
-            <ModalCentral>
-              <div>
-              <InputNomeModal>
-                <label>CEP</label>
-                <TextField size='small' variant="outlined" {...register("address.zipCode")} onChange={(e) => handleCep(e.target.value)} />
-                </InputNomeModal>
-              </div>
-              <div>
-              <InputNomeModal>
-                <label>Rua</label>
-                <TextField sx={{ width: '500px', color: 'var(--text)' }} size='small' id="outlined-basic" variant="outlined" {...register("address.street")} />
+                  <TextField
+                    size="small"
+                    id="phone"
+                    type="number"
+                    variant="outlined"
+                    {...register('phone')}
+                  />
                 </InputNomeModal>
               </div>
             </ModalCentral>
             <ModalCentral>
               <div>
-              <InputNomeModal>
-                <label>Bairro</label>
-                <TextField sx={{ width: '360px' }} size='small' variant="outlined" {...register("address.neighborhood")} />
+                <InputNomeModal>
+                  <label>Email</label>
+                  <TextField
+                    sx={{ width: '500px', color: 'var(--text)' }}
+                    size="small"
+                    id="email"
+                    variant="outlined"
+                    {...register('email')}
+                  />
                 </InputNomeModal>
               </div>
               <div>
-              <InputNomeModal>
-                <label>Número</label>
-                <TextField size='small' id="outlined-basic" variant="outlined" {...register("address.number")} />
-                </InputNomeModal>
-              </div>
-              <div>
-              <InputNomeModal>
-                <label>Hotel</label>
-                <TextField size='small' id="outlined-basic" variant="outlined" {...register("hotel_id")} />
-                </InputNomeModal>
-              </div>
-            </ModalCentral>
-            <ModalCentral>
-              <div>
-              <InputNomeModal>
-                <label>Cidade</label>
-                <TextField sx={{ width: '460px' }} size='small' id="cpf" type="text" variant="outlined" maxRows={11} {...register("address.city")} />
-                </InputNomeModal>
-              </div>
-              <div>
-              <InputNomeModal>
-                <label>UF</label>
-                <TextField sx={{ width: '100px' }} size='small' id="cpf" type="text" variant="outlined" maxRows={11} {...register("address.state")} />
-                </InputNomeModal>
-              </div>
-              <div>
-              <InputNomeModal>
-                <label>Complemento</label>
-                <TextField size='small' id="cpf" type="text" variant="outlined" maxRows={11} {...register("address.complement")} />
+                <InputNomeModal>
+                  <label>Senha</label>
+                  <TextField
+                    size="small"
+                    type="number"
+                    variant="outlined"
+                    maxRows={11}
+                    {...register('password')}
+                  />
                 </InputNomeModal>
               </div>
             </ModalCentral>
 
-            <Button sx={{bgcolor: 'var(--text)'}} variant="contained" type='submit'  >Enviar</Button>
-            <Button variant="contained" color="error" onClick={close}>Cancelar</Button>
+            <ModalCentral>
+              <div>
+                <InputNomeModal>
+                  <label>CEP</label>
+                  <TextField
+                    size="small"
+                    variant="outlined"
+                    {...register('address.zipCode')}
+                    onChange={(e) => handleCep(e.target.value)}
+                  />
+                </InputNomeModal>
+              </div>
+              <div>
+                <InputNomeModal>
+                  <label>Rua</label>
+                  <TextField
+                    sx={{ width: '500px', color: 'var(--text)' }}
+                    size="small"
+                    id="outlined-basic"
+                    variant="outlined"
+                    {...register('address.street')}
+                  />
+                </InputNomeModal>
+              </div>
+            </ModalCentral>
+            <ModalCentral>
+              <div>
+                <InputNomeModal>
+                  <label>Bairro</label>
+                  <TextField
+                    sx={{ width: '360px' }}
+                    size="small"
+                    variant="outlined"
+                    {...register('address.neighborhood')}
+                  />
+                </InputNomeModal>
+              </div>
+              <div>
+                <InputNomeModal>
+                  <label>Número</label>
+                  <TextField
+                    size="small"
+                    id="outlined-basic"
+                    variant="outlined"
+                    {...register('address.number')}
+                  />
+                </InputNomeModal>
+              </div>
+              <div>
+                <InputNomeModal>
+                  <label>Hotel</label>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="hotel"
+                    {...register('hotel_id')}
+                    onChange={handleChange}
+                  >
+                  <MenuItem value=""  selected >Selecione...</MenuItem> 
+                    {hotels.map((hotel) => (
+
+                      <MenuItem value={hotel.id}>{hotel.name}</MenuItem>
+                    ))}
+                  </Select>
+                </InputNomeModal>
+              </div>
+            </ModalCentral>
+            <ModalCentral>
+              <div>
+                <InputNomeModal>
+                  <label>Cidade</label>
+                  <TextField
+                    sx={{ width: '460px' }}
+                    size="small"
+                    id="cpf"
+                    type="text"
+                    variant="outlined"
+                    maxRows={11}
+                    {...register('address.city')}
+                  />
+                </InputNomeModal>
+              </div>
+              <div>
+                <InputNomeModal>
+                  <label>UF</label>
+                  <TextField
+                    sx={{ width: '100px' }}
+                    size="small"
+                    id="cpf"
+                    type="text"
+                    variant="outlined"
+                    maxRows={11}
+                    {...register('address.state')}
+                  />
+                </InputNomeModal>
+              </div>
+              <div>
+                <InputNomeModal>
+                  <label>Complemento</label>
+                  <TextField
+                    size="small"
+                    id="cpf"
+                    type="text"
+                    variant="outlined"
+                    maxRows={11}
+                    {...register('address.complement')}
+                  />
+                </InputNomeModal>
+              </div>
+            </ModalCentral>
+
+            <Button
+              sx={{ bgcolor: 'var(--text)' }}
+              variant="contained"
+              type="submit"
+            >
+              Enviar
+            </Button>
+            <Button variant="contained" color="error" onClick={close}>
+              Cancelar
+            </Button>
           </form>
         </div>
       </Box>
     </ModalExterna>
   )
 }
-         
